60e33c7d6c2933c7757b48f5ffe624093fd84678
심요셉 <s.yosep@kakao.com>
2024-09-27 17:48:01 +0900

    add js

--- src/App.js
+++ src/App.js
@@ -3,21 +3,28 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const [fontSize, setFontSize] = useState(16);
+  const [fontSize, setFontSize] = useState(16); // 초기 폰트 크기 설정
 
   const handleMouseEnter = () => {
-    setFontSize(prevSize => prevSize +5);
+    setFontSize(prevSize => prevSize + 5); // 마우스가 들어오면 폰트 크기 증가
   };
 
   const handleMouseLeave = () => {
-    setFontSize(16);
-  }
+    setFontSize(16); // 마우스가 나가면 폰트 크기 초기화
+  };
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p id="hello">Hello World!!!</p>
+        <p
+          id="hello"
+          style={{ fontSize: `${fontSize}px`, transition: 'font-size 0.3s ease' }}
+          onMouseEnter={handleMouseEnter} // 마우스가 들어오면 호출
+          onMouseLeave={handleMouseLeave} // 마우스가 나가면 호출
+        >
+          Hello World!!!
+        </p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
