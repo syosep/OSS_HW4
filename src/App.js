604541c071c24f6bbbe980056b2edf3569c0eaec
심요셉 <s.yosep@kakao.com>
2024-09-27 17:38:27 +0900

    add js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [fontSize, setFontSize] = useState(16);
+
+  const handleMouseEnter = () => {
+    setFontSize(prevSize => prevSize +5);
+  };
+
+  const handleMouseLeave = () => {
+    setFontSize(16);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
